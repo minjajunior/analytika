44986b9080d5f75307df6033d2b42aed67805add
Minja Junior <mailgiven@gmail.com>
2019-05-09 14:37:36 +0300

    Initial commit

---
+++ application/views/home_view.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <!-- Site Title-->
+    <title>Home</title>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <script src="../cdn-cgi/apps/head/3ts2ksMwXvKRuG480KNifJ2_JNM.js"></script>
+    <!-- Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900,300italic">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css')?>">
+    <!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="https://windows.microsoft.com/en-US/internet-explorer/">
+<img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<!-- Page-->
+<div class="page">
+    <div class="page-loader">
+        <div class="cssload-container">
+            <div class="cssload-speeding-wheel"></div>
+            <p class="loader-header">Loading</p>
+        </div>
+    </div>
+    <!-- Page Header-->
+    <header class="page-head">
+        <!-- RD Navbar-->
+        <div class="rd-navbar-wrap">
+            <nav class="rd-navbar rd-navbar-light" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-static" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-lg-stick-up-clone="true" data-md-stick-up-offset="80px" data-lg-stick-up-offset="80px" data-stick-up="true" data-sm-stick-up="true" data-md-stick-up="true" data-lg-stick-up="true">
+                <div class="rd-navbar-inner">
+                    <div class="rd-navbar-inner-body">
+                        <!-- RD Navbar Panel-->
+                        <div class="rd-navbar-panel">
+                            <!-- RD Navbar Toggle-->
+                            <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                            <!-- RD Navbar Brand-->
+                            <a href="<?php echo base_url()?>">
+                                <img src="<?php echo base_url('assets/images/logo.png')?>" alt="" />
+                            </a>
+                        </div>
+                        <div class="rd-navbar-nav-wrap">
+                            <!-- RD Navbar Nav-->
+                            <ul class="rd-navbar-nav">
+                                <li class="active"><a href="<?php echo base_url()?>">HOME</a></li>
+                                <li><a href="<?php echo base_url()?>">ABOUT US</a></li>
+                                <li><a href="<?php echo base_url()?>">SERVICES</a></li>
+                                <li><a href="<?php echo base_url()?>">CONTACTS</a></li>
+                            </ul>
+                        </div>
+                        <div class="block-right">
+                            <ul class="list-inline">
+                                <li><a class="icon icon-xxs icon-circle icon-dark icon-alto-filled fa-facebook novi-icon" href="#"></a></li>
+                                <li><a class="icon icon-xxs icon-circle icon-dark icon-alto-filled fa-twitter novi-icon" href="#"></a></li>
+                                <li><a class="icon icon-xxs icon-circle icon-dark icon-alto-filled fa-google-plus novi-icon" href="#"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <!-- Swiper-->
+    <section class="swiper-container swiper-slider swiper-slider-novi" data-min-height="400px">
+        <div class="swiper-wrapper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide bg-gray-darker" data-slide-bg="<?php echo base_url('assets/images/top.jpg')?>">
+                    <div class="swiper-slide-caption">
+                        <!-- Solutions that never miss the aim-->
+                        <div class="section-68 section-sm-top-125 section-sm-bottom-152 section-lg-top-125 section-lg-bottom-235">
+                            <div class="swiper-content shell text-center text-sm-left">
+                                <div class="range range-fix">
+                                    <div class="cell-xs-7 cell-md-preffix-1 cell-xl-preffix-0 pull-right">
+                                        <h3>Today insights, secure Tomorrow opportunities</h3>
+                                        <p class="big">Safari Analytika supporting businesses to make well informed decisions in a timely manner,<br class="veil reveal-sm-block">ensuring tomorrow opportunities are secured ahead of time. </p>
+                                        <div class="group-md offset-top-25"><a class="btn btn-sm btn-curious-blue-variant-1 min-width-160" href="<?php echo base_url()?>">About Us</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Main services-->
+    <section class="section-top-60 section-bottom-55 novi-background bg-cover">
+        <div class="shell">
+            <div class="range range-fix text-center">
+                <div class="cell-xs-12">
+                    <h2>Our Services</h2>
+                    <hr class="hr-block hr-curious-blue">
+                </div>
+            </div>
+            <div class="range range-30 range-md-60 range-sm-center">
+                <div class="cell-xs-12 cell-sm-5 cell-lg-3">
+                    <div class="product">
+                        <div class="icon-decoration-border">
+                            <div class="icon-decoration-border-inner"><span class="icon icon-md-variant-2 icon-primary fl-bigmug-line-checkmark16 novi-icon"></span></div>
+                        </div>
+                        <h4><a href="#">Data analytics</a></h4>
+                        <p><a class="link link-black link-medium link-icon link-icon-right" href="<?php echo base_url()?>"><span class="icon icon-xs icon-curious-blue fa-angle-right"></span>Read more</a></p>
+                    </div>
+                </div>
+                <div class="cell-xs-12 cell-sm-5 cell-lg-3">
+                    <div class="product">
+                        <div class="icon-decoration-border">
+                            <div class="icon-decoration-border-inner"><span class="icon icon-md-variant-2 icon-primary fl-bigmug-line-sand14 novi-icon"></span></div>
+                        </div>
+                        <h4><a href="#">IFRS 9 modelling </a></h4>
+                        <p><a class="link link-black link-medium link-icon link-icon-right" href="<?php echo base_url()?>"><span class="icon icon-xs icon-curious-blue fa-angle-right"></span>Read more</a></p>
+                    </div>
+                </div>
+                <div class="cell-xs-12 cell-sm-5 cell-lg-3">
+                    <div class="product product-inset-1">
+                        <div class="icon-decoration-border">
+                            <div class="icon-decoration-border-inner">
+                                <div class="icon icon-image"><span class="icon icon-md-variant-2 icon-primary fl-bigmug-line-three142 novi-icon"></span></div>
+                            </div>
+                        </div>
+                        <h4><a href="#">CRD Data Submission</a></h4>
+                        <p><a class="link link-black link-medium link-icon link-icon-right" href="<?php echo base_url()?>"><span class="icon icon-xs icon-curious-blue fa-angle-right"></span>Read more</a></p>
+                    </div>
+                </div>
+                <div class="cell-xs-12 cell-sm-5 cell-lg-3">
+                    <div class="product product-inset-1">
+                        <div class="icon-decoration-border">
+                            <div class="icon-decoration-border-inner">
+                                <div class="icon icon-image"><span class="icon icon-md-variant-2 icon-primary fl-bigmug-line-portfolio23 novi-icon"></span></div>
+                            </div>
+                        </div>
+                        <h4><a href="#">Financial Advice</a></h4>
+                        <p><a class="link link-black link-medium link-icon link-icon-right" href="<?php echo base_url()?>"><span class="icon icon-xs icon-curious-blue fa-angle-right"></span>Read more</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="section section-35 section-sm-68 novi-background">
+        <div class="shell">
+            <div class="range range-50 offset-top-25 offset-sm-top-56">
+                <div class="cell-xs-12 cell-sm-6">
+                    <div class="service">
+                        <div class="service-body">
+                            <h4 class="text-bold"><a href="#">Safari Analytika Training</a></h4>
+                            <hr class="hr-block hr-left hr-curious-blue"/>
+                            <p>Safari Analytika provides training and development courses, both generic and customized, related to IFRS 9, credit bureaus data submission and credit lifecycle management. </p>
+                            <p><a class="link link-black link-medium link-icon link-icon-right" href="<?php echo base_url()?>"><span class="icon icon-xs icon-curious-blue fa-angle-right"></span>Read more</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="cell-xs-12 cell-sm-6">
+                    <div class="service">
+                        <div class="service-image">
+                            <figure><img src="<?php echo base_url('assets/images/bg.jpg')?>" alt="" /></figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="shell">
+            <div class="range range-50 offset-top-25 offset-sm-top-56">
+                <div class="cell-xs-12 cell-sm-6">
+                    <div class="service">
+                        <div class="service-image">
+                            <figure><img src="<?php echo base_url('assets/images/top.jpg')?>" alt="" /></figure>
+                        </div>
+                    </div>
+                </div>
+                <div class="cell-xs-12 cell-sm-6">
+                    <div class="service">
+                        <div class="service-body">
+                            <h4 class="text-bold"><a href="#">Credit Report Assistance</a></h4>
+                            <hr class="hr-block hr-left hr-curious-blue"/>
+                            <p>
+                            <ul>
+                                <li><a href="#">Get your free Annual Credit Report</a></li>
+                                <li><a href="#">Disputes</a></li>
+                                <li><a href="#">Denied Credit</a></li>
+                                <li><a href="#">Opt out of Pre-screened Offers</a></li>
+                            </ul>
+                            <a class="link link-black link-medium link-icon link-icon-right" href="<?php echo base_url()?>"><span class="icon icon-xs icon-curious-blue fa-angle-right"></span>Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Page Footer-->
+    <footer class="page-foot">
+        <div class="section-45 section-sm-75 bg-river-bed novi-background bg-cover">
+            <div class="shell text-center text-sm-left">
+                <div class="range range-45 range-xs-center">
+                    <div class="cell-xs-12 cell-sm-6 cell-lg-4">
+                        <h4>About Us</h4>
+                        <hr class="hr-variant-1">
+                        <div class="footer-nav-column">
+                            <ul class="footer-nav">
+                                <li><a href="<?php echo base_url()?>">Home</a></li>
+                                <li><a href="<?php echo base_url()?>">About</a></li>
+                                <li><a href="<?php echo base_url()?>">Service</a></li>
+                                <li><a href="<?php echo base_url()?>">Corporate responsibility</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="cell-xs-12 cell-sm-6 cell-lg-4">
+                        <h4>Legal</h4>
+                        <hr class="hr-variant-1">
+                        <div class="footer-nav-column">
+                            <ul class="footer-nav">
+                                <li><a href="<?php echo base_url()?>">Privacy policy</a></li>
+                                <li><a href="<?php echo base_url()?>">Email Disclaimer</a></li>
+                                <li><a href="<?php echo base_url()?>">Terms and Conditions</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="cell-xs-12 cell-sm-6 cell-lg-4">
+                        <h4>Contact info</h4>
+                        <hr class="hr-variant-1">
+                        <address class="contact-info reveal-inline-block">
+                            <ul class="contact-info-list">
+                                <li class="unit unit-horizontal unit-spacing-md">
+                                    <div class="unit-left"><span class="icon icon-sm icon-picton-blue fa-phone novi-icon"></span></div>
+                                    <div class="unit-body"><span class="text-bold"><a href="tel:#">+255 22 2110535</a></span></div>
+                                </li>
+                                <li class="unit unit-horizontal unit-spacing-md">
+                                    <div class="unit-left"><span class="icon icon-sm icon-picton-blue fa-envelope-o novi-icon"></span></div>
+                                    <div class="unit-body"><span>info@safarianalytika.co.tz</span></div>
+                                </li>
+                                <li class="unit unit-horizontal unit-spacing-md">
+                                    <div class="unit-left"><span class="icon icon-sm icon-picton-blue fa-clock-o novi-icon"></span></div>
+                                    <div class="unit-body"><span>Mon - Fri: 8:30 - 16:30</span></div>
+                                </li>
+                                <li class="unit unit-horizontal unit-spacing-md">
+                                    <div class="unit-left"><span class="icon icon-sm icon-picton-blue fa-map-marker novi-icon"></span></div>
+                                    <div class="unit-body">
+                                        <span>
+                                            <a href="#">
+                                                <span class="text-bold">Block No. 106, 1st Floor Tancot House,</span>
+                                                <span>Sokoine Drive</span>
+                                            </a>
+                                        </span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </address>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section-14 bg-ebony-clay novi-background bg-cover">
+            <div class="shell text-center">
+                <p class="rights">&#169;&nbsp;<span class="copyright-year"></span>&nbsp;Safari Analytika.</p>
+            </div>
+        </div>
+    </footer>
+</div>
+
+<!-- Javascript-->
+<script src="<?php echo base_url('assets/js/core.min.js')?>"></script>
+<script src="<?php echo base_url('assets/js/script.js')?>"></script>
+
+</body>
+</html>
