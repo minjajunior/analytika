c5828673541afa37ad91a26e900f8120c755c6f4
dev-felx <felixdevidf@gmail.com>
2019-07-08 19:12:29 +0300

    Training added header

---
+++ application/views/shared/header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <!-- Site Title-->
+    <title>Home</title>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <!-- Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900,300italic">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css')?>">
+
+    <!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="https://windows.microsoft.com/en-US/internet-explorer/">
+        <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<!-- Page-->
+<div class="page">
+    <div class="page-loader">
+        <div class="cssload-container">
+            <div class="cssload-speeding-wheel"></div>
+            <p class="loader-header">Loading</p>
+        </div>
+    </div>
+    <!-- Page Header-->
+    <header class="page-head">
+
+        <!-- RD Navbar-->
+        <div class="rd-navbar-wrap">
+
+            <nav class="rd-navbar rd-navbar-light" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-static" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-lg-stick-up-clone="true" data-md-stick-up-offset="80px" data-lg-stick-up-offset="80px" data-stick-up="true" data-sm-stick-up="true" data-md-stick-up="true" data-lg-stick-up="true">
+
+               <div class="rd-navbar-inner">
+
+                   <div class="rd-navbar-inner-body">
+                        <div class="rd-navbar-panel">
+                            <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                            <div class="rd-navbar-brand hidden-sm-down">
+                                <a href="<?php echo base_url()?>">
+                                    <img class="brand-name" src="<?php echo base_url('assets/images/logo3.png')?>" alt="Logo" />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="rd-navbar-nav-wrap hidden-sm-down ">
+
+                            <ul class="rd-navbar-nav">
+                                <li class="active"><a href="<?php echo base_url()?>">HOME</a></li>
+                                <li><a href="<?php echo base_url('about')?>">ABOUT US</a></li>
+                                <li><a href="<?php echo base_url('service')?>">SERVICES</a></li>
+                                <li><a href="<?php echo base_url('training')?>">TRAININGS</a></li>
+                                <li><a href="<?php echo base_url('contact')?>">CONTACTS</a></li>
+                            </ul>
+
+                            <ul class="rd-navbar-nav">
+                                <li><a class="icon icon-xs icon-circle icon-dark icon-alto-filled fa-facebook " href="#"></a></li>
+                                <li><a class="icon icon-xs icon-circle icon-dark icon-alto-filled fa-twitter " href="#"></a></li>
+                                <li><a class="icon icon-xs icon-circle icon-dark icon-alto-filled fa-instagram n" href="#"></a></li>
+
+                            </ul>
+
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+
+
+            </nav>
+
+        </div>
+    </header>
